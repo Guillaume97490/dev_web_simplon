63a27c2165ed944da5a0a10b4b13f93e8608f43c
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-10-07 22:47:58 +0400

    edit controller & list.ejs

--- NodeJs/exo5-Calculate/controllers/calculateController.js
+++ NodeJs/exo5-Calculate/controllers/calculateController.js
@@ -1,25 +1,32 @@
 const controller = {};
 const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
-
 var ObjectId = require('mongodb').ObjectID;
 
+// CONNECT TO COLLECTION
+function connect(){
+  mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {
+  if (err) {
+    console.error(err);return
+  }
+
+  db = client.db('calculate_db')
+  collection = db.collection('calculate')
+  });
+}
+var collection = connect();
+
+  
 
 controller.list = (req, res) => {
-    mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {if (err) {console.error(err);return}
-  
-    // CONNECT TO COLLECTION
-    const db = client.db('calculate_db')
-    const collection = db.collection('calculate')
-  
     // FIND ALL DOCUMENTS IN A COLLECTION
     collection.find().toArray((err, items) => {
-      // console.log(items)s
+      // console.log(items)
       res.render('list.ejs', {
-        data: items
+        data: items,
+        // errorMsg: 'Chiffre zéro ou opérateur inconue'
         });
       });
-    });
   };
 
   controller.save = (req, res) => {
@@ -30,7 +37,6 @@ controller.list = (req, res) => {
 
     switch (req.body.operator) {
       case '0':
-          // console.log('test');
           operator = '+';
           break;
 
@@ -51,14 +57,16 @@ controller.list = (req, res) => {
     }
 
     if(number1 == 0 || number2 == 0 || operator == "error"){
-      res.redirect("/calculate");
+
+      collection.find().toArray((err, items) => {
+        // console.log(items)
+        res.render('list.ejs', {
+          data: items,
+          errorMsg: 'Chiffre zéro non acceptée ou opérateur incorrect'
+          });
+        });
     };
 
-    mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {if (err) {console.error(err);return}
-    
-      // CONNECT TO COLLECTION
-      const db = client.db('calculate_db')
-      const collection = db.collection('calculate')
       if (number1 && number2 && operator){
         // INSERT DATA INTO A COLLECTION
         collection.insertOne(
@@ -76,7 +84,6 @@ controller.list = (req, res) => {
       else {
         // console.log('erreur')
       }
-    })
   };
 
 
@@ -84,28 +91,23 @@ controller.list = (req, res) => {
     // console.log(req.params.id)
 
 
-    mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {if (err) {console.error(err);return}
-      // get a collection
-      const db = client.db('calculate_db')
-      const collection = db.collection('calculate')
+    // FIND ONE DOCUMENT
+    collection.findOne({ _id: ObjectId(req.params.id)}, (err, item) => {
+      console.log(item)
+      number1 = Number(item.number1);
+      number2 = Number(item.number2);
+      var operator = item.operator;
 
-      var number1 = ''
-      var number2 = ''
-      var operator = ''
-      var result = ''
+      if (item.disabled == '1'){
+        collection.find().toArray((err, items) => {
+          // console.log(items)
+          res.render('list.ejs', {
+            data: items,
+            errorMsg: 'Cette opération a été désactiver'
+            });
+          });
+      }
 
-
-        // FIND ONE DOCUMENT
-      collection.findOne({ _id: ObjectId(req.params.id)}, (err, item) => {
-        // console.log(item)
-        number1 = Number(item.number1);
-        number2 = Number(item.number2);
-        var operator = item.operator;
-
-
-        
-
-      // console.log(operator)
       switch (operator) {
         case '+':
             result = number1 + number2;
@@ -125,18 +127,13 @@ controller.list = (req, res) => {
 
         default:
             result = "Un probleme est survenue";
-    }
-    if (item.disabled == 1){
-      res.redirect("/calculate");
-    }
-
+      }
 
       // UPDATE A DOCUMENT
       if (req.params.id){
         collection.updateOne({_id: ObjectId(req.params.id)}, {'$set': {
           result: result
         }}, (err, item) => {
-          // console.log(item)
           res.redirect("/calculate");
         });
       }
@@ -147,31 +144,31 @@ controller.list = (req, res) => {
 
     });
 
-
-  });
-
   }
 
 
   controller.edit = (req, res) => {
-    mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {if (err) {console.error(err);return}
-     // CONNECT TO COLLECTION
-      const db = client.db('calculate_db')
-      const collection = db.collection('calculate')
-      // FIND ONE DOCUMENT
-      collection.findOne({ _id: ObjectId(req.params.id)}, (err, item) => {
-        // console.log(item)
-        if (item){
-          collection.find().toArray((err, items) => {
-            // console.log(items)
-            res.render('list.ejs', {
-              data: items,
-              dataEdit: item
-              });
+    collection.findOne({ _id: ObjectId(req.params.id)}, (err, item) => {
+      console.log(item)
+      if (item.disabled == '1'){
+        collection.find().toArray((err, items) => {
+          // console.log(items)
+          res.render('list.ejs', {
+            data: items,
+            errorMsg: 'Cette operation à été désactiver'
             });
-          
-        };
-      });
+          });
+      }
+      
+      if (item){
+        collection.find().toArray((err, items) => {
+          // console.log(items)
+          res.render('list.ejs', {
+            data: items,
+            dataEdit: item
+          });
+        });
+      };
     });
   };
 
@@ -185,7 +182,6 @@ controller.list = (req, res) => {
 
     switch (req.body.operator) {
       case '0':
-          // console.log('test');
           operator = '+';
           break;
 
@@ -206,75 +202,47 @@ controller.list = (req, res) => {
     }
 
     if(number1 == 0 || number2 == 0 || operator == "error"){
-      res.redirect("/calculate");
+
+      collection.find().toArray((err, items) => {
+        // console.log(items)
+        res.render('list.ejs', {
+          data: items,
+          errorMsg: 'Chiffre zéro non acceptée ou opérateur incorrect'
+          });
+        });
     };
     
-    mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {if (err) {console.error(err);return}
-        // get a collection
-        const db = client.db('calculate_db')
-        const collection = db.collection('calculate')
-        // UPDATE A DOCUMENT
-        if (req.body.id){
-          collection.updateOne({_id: ObjectId(req.body.id)}, {'$set': {
-            number1: number1,
-            number2: number2,
-            operator: operator,
-            result: ''
-          }}, (err, item) => {
-            // console.log(item)
-            res.redirect("/calculate");
-          });
-        }
-        else{
-          // console.log('erreur')
-        }
-    });
+    
+    if (req.body.id){
+      collection.updateOne({_id: ObjectId(req.body.id)}, {'$set': {
+        number1: number1,
+        number2: number2,
+        operator: operator,
+        result: ''
+      }}, (err, item) => {
+        // console.log(item)
+        res.redirect("/calculate");
+      });
+    }
+    else{
+      // console.log('erreur')
+    }
   };
 
 
   controller.disable = (req, res) => {
-    // console.log(req);
 
-    
-    mongo.connect(url, {useNewUrlParser: true,useUnifiedTopology: true}, (err, client) => {if (err) {console.error(err);return}
-        // get a collection
-        const db = client.db('calculate_db')
-        const collection = db.collection('calculate')
-        // UPDATE A DOCUMENT
-        if (req.params.id){
-          collection.updateOne({_id: ObjectId(req.params.id)}, {'$set': {
-            disabled: 1
-          }}, (err, item) => {
-            // console.log(item)
-            res.redirect("/calculate");
-          });
-        }
-        else{
-          // console.log('erreur')
-        }
-    });
+    // UPDATE A DOCUMENT
+    if (req.params.id){
+      collection.updateOne({_id: ObjectId(req.params.id)}, {'$set': {
+        disabled: 1
+      }}, (err, item) => {
+        res.redirect("/calculate");
+      });
+    }
+    else{
+      // console.log('erreur')
+    }
   };
-
-
-  // controller.delete = (req, res) => {
-  //   mongo.connect(url, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true
-  //     }, (err, client) => {
-  //     if (err) {
-  //       console.error(err)
-  //       return
-  //     }
-  //     // get a collection
-  //     const db = client.db('livres_db')
-  //     const collection = db.collection('livres')
-  //     // DELETE A DOCUMENT
-  //     collection.deleteOne({_id: ObjectId(req.params.id)}, (err, item) => {
-  //       console.log(item)
-  //       res.redirect("/livre");
-  //     })
-  //   });
-  // };
-
 
 module.exports = controller;
