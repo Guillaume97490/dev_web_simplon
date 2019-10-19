c3135487892b7db8d275343ac64c68df2a2e5008
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-10-19 16:52:59 +0400

    add favicon & 404 error page

--- A-presenter/Calculatrice/app.js
+++ A-presenter/Calculatrice/app.js
@@ -1,6 +1,12 @@
+// Requires 
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+
+// importing routes
+const calculRoutes = require('./routes/calcul');
+const technoRoutes = require('./routes/techno')
+
 
 // Body parser 
 app.use(bodyParser.urlencoded({
@@ -8,23 +14,25 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+
 app.use(express.static(__dirname + '/public'));
-
-
-// importing routes
-const calculRoutes = require('./routes/calcul');
-const technoRoutes = require('./routes/techno')
-
-
-// settings
-app.set('port', process.env.PORT || 3000);
 
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // routes
 app.use('/calcul', calculRoutes);
 app.use('/technologie', technoRoutes);
+
+
+// Erreur 404
+app.use(function(req,res){
+    res.status(404).render('404');
+});
+
+// settings
+app.set('port', process.env.PORT || 3000);
 
 // starting the server
 app.listen(app.get('port'), () => {
